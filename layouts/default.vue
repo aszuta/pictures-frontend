199ca05ffd15b79c961064325e08b7b412143377
unknown <adamszuta@vp.pl>
2022-05-28 18:50:21 +0200

    Deleted styles for layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,27 +10,3 @@ export default {
   name: 'PageLayout',
 };
 </script>
-
-<style lang="scss">
-// @import url("https://fonts.googleapis.com/css2?family=Rubik&display=swap");
-
-* {
-  margin: 0;
-  padding: 0;
-  border: none;
-  box-sizing: border-box;
-  font-family: inherit;
-  text-decoration: inherit;
-  font-family: Rubik;
-}
-
-html, body {
-  overflow-x: hidden;
-}
-
-.App_layout {
-  width: 100%;
-  min-height: 100vh;
-  background-color: #262626;
-}
-</style>
